213b42c06d622e003abd9253ff4a937404ad9acd
Imami-syed <syed.i@research.iiit.ac.in>
2023-06-25 22:04:36 +0530

    1200 commit 1 😄

--- 1200/AppendforOR.cpp
+++ 1200/AppendforOR.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
-using namespace std;
 #define ll long long int
+using namespace std;
 int main(){
     ll t;
     cin >> t;
     while(t--){
-        ll n;
-        cin >> n;
+        ll n,y;
+        cin >> n >> y;
         ll a[n];
         for(ll i=0;i<n;i++)
         {
