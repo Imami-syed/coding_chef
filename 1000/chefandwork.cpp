e3c0129805dc49c77a43cadd7a1fd4f50cff08e8
Imami-syed <syed.i@research.iiit.ac.in>
2023-06-25 21:59:50 +0530

    1000 done😜

--- 1000/chefandwork.cpp
+++ 1000/chefandwork.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #define ll long long int
 using namespace std;
 int main(){
@@ -8,18 +9,29 @@ int main(){
         ll n , k;
         cin >> n >> k;
         ll w[n];
-        ll cntcry=0, sum=0;
+        ll maxm=0;
         for(ll i=0;i<n;i++)
         {
             cin >> w[i];
-            if(w[i] > k) cntcry++;
-            sum+=w[i];
+            maxm=max(maxm,w[i]);
         }
-        if(cntcry>0){
+        if(maxm>k){
             cout << -1 << endl; 
         }
         else{
-          
+            ll cnt=0,i=0;
+            while(true){
+                ll sum=0;
+                while(sum+w[i]<=k){
+                    sum+=w[i];
+                    i++;
+                }
+                cnt++;
+                if(i>n-1){
+                    cout << cnt << endl;
+                    break;
+                }
+            }
         }
 
     }
