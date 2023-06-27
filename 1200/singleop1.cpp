658b2971517ce5be97961a737158c3bbdf964205
Imami-syed <syed.i@research.iiit.ac.in>
2023-06-27 16:10:04 +0530

    1200 commit 5 😄

--- 1200/singleop1.cpp
+++ 1200/singleop1.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <string>
+#include <algorithm>
 #define ll long long int
 using namespace std;
+ll bintoint(string s){
+    ll res;
+    // 100101
+    for(ll i=s.length()-1;i>=0;i--)
+    {
+        ll temp=0;
+        char charc=s[i];
+        temp=pow(2,temp)*atoi(charc);
+
+    }
+    return res;
+}
 int main(){
     ll t;
     cin >> t;
